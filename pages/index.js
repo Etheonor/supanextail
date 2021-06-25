e082e8fba8c3d9fe1b987ab69e5a07e644000249
Michael <etheanore@gmail.com>
2021-06-25 12:01:13 +0200

    Add open graph header for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/*Don't forget to modify the NextSeo component with your website informations */
+
 import Landing from "components/Landing";
 import Layout from "components/Layout";
 import { NextSeo } from "next-seo";
@@ -8,6 +10,27 @@ const Home = () => {
       <NextSeo
         title={`Welcome to ${process.env.NEXT_PUBLIC_TITLE} 👋`}
         description={`SupaNexTail is a boilerplate for your website, based on Next.js, Supabase, and TailwindCSS`}
+        openGraph={{
+          type: "website",
+          url: "https://www.supanextail.com/",
+          title: `Welcome to ${process.env.NEXT_PUBLIC_TITLE} 👋`,
+          description:
+            "SupaNexTail is a boilerplate for your website, based on Next.js, Supabase, and TailwindCSS",
+          images: [
+            {
+              url: "https://www.supanextail.com/ogimage.png",
+              width: 1200,
+              height: 630,
+              alt: "SupaNexTail",
+            },
+            {
+              url: "https://www.supanextail.com/ogimage.png",
+              width: 1200,
+              height: 630,
+              alt: "SupaNexTail",
+            },
+          ],
+        }}
       />
       <Layout>
         <Landing />
