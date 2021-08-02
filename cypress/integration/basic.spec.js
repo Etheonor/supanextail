a7eb41228b7ed965dcc864246585422c891d5270
Michael <etheanore@gmail.com>
2021-08-02 11:53:42 +0200

    Clean GH workflow and add another test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -2,5 +2,8 @@ describe('Homepage Test', () => {
     it('Visits the homepage', () => {
       cy.visit('')
     })
+    it('Visits the pricing page', () => {
+        cy.visit('/pricing')
+    })
   })
   
