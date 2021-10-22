ef01ece55fbc3442a7b048c804fa221f10bf1c56
Michael <mbreyton@amplitude-studios.com>
2021-10-22 17:17:30 +0200

    Rework e2e test (playwright) - Test ci flow

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
+	console.log(process.env.PLAYWRIGHT_TEST_BASE_URL);
 	await page.goto('https://playwright.dev/');
 	const title = page.locator('.navbar__inner .navbar__title');
 	await expect(title).toHaveText('Playwright');
